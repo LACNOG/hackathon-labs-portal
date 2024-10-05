013136a38efd3e5de3b7bdb1ef043c9473a59aab
Carlos Martinez <carlos@cagnazzo.uy>
2024-10-05 13:52:02 -0400

    H1

--- student_portal/portal/urls.py
+++ student_portal/portal/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.urls import path, re_path, include
+from .views import CustomConfirmEmailView, CustomEmailVerificationSentView
 from . import views
+
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('register/', views.student_signup, name='register'),
     path('dashboard/', views.student_dashboard, name='student_dashboard'),
     # path('import-lab-equipment/<int:workshop_id>/', import_lab_equipment, name='import_lab_equipment'),
-]
+
+    path('accounts/email-verification-sent/', views.CustomEmailVerificationSentView.as_view(), name='account_email_verification_sent'),
+    path('accounts/email-verification-instructions/', views.EmailVerificationInstructionsView.as_view(), name='email_verification_instructions'),
+    re_path(r'^accounts/confirm-email/(?P<key>[-:\w]+)/$', views.CustomConfirmEmailView.as_view(), name='account_confirm_email'),
+    path('accounts/', include('allauth.urls')),    
+]       
+ 
--- student_portal/portal/views.py
+++ student_portal/portal/views.py
@@ -2,6 +2,41 @@ from django.shortcuts import render
 from .forms import StudentRegistrationForm
 from allauth.account.views import SignupView
 from .models import StudentProfile, Workshop
+from django.urls import reverse
+from allauth.account.views import EmailVerificationSentView, ConfirmEmailView
+from django.views.generic import TemplateView
+from allauth.account.models import EmailConfirmation, EmailConfirmationHMAC
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.contrib import messages
+
+from django.views.decorators.debug import sensitive_post_parameters
+
+
+class CustomConfirmEmailView(ConfirmEmailView):
+    template_name = 'account/email_confirmation.html'
+
+    def get_object(self, *args, **kwargs):
+        key = kwargs.get('key')
+        emailconfirmation = EmailConfirmationHMAC.from_key(key)
+        if not emailconfirmation:
+            if EmailConfirmation.objects.all_valid().filter(key=key).exists():
+                emailconfirmation = EmailConfirmation.objects.all_valid().get(key=key)
+                emailconfirmation.confirm(self.request)
+            else:
+                emailconfirmation = None
+        return emailconfirmation
+
+    def get(self, *args, **kwargs):
+        self.object = self.get_object(**kwargs)
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['confirmation'] = self.object
+        return context
+
 
 class StudentSignupView(SignupView):
     template_name = 'registration/register.html'
@@ -18,3 +53,13 @@ def landing_page(request):
         'open_workshops': open_workshops
     }
     return render(request, "portal/landing_page.html", context)
+
+class CustomEmailVerificationSentView(EmailVerificationSentView):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'account/email_verification_sent_initial.html', {'messages': messages.get_messages(request)})
+    
+class CustomConfirmEmailView(ConfirmEmailView):
+    template_name = 'account/email_confirmation.html'
+
+class EmailVerificationInstructionsView(TemplateView):
+    template_name = 'account/email_verification_instructions.html'
